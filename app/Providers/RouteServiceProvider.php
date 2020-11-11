c900a8e63adecf9d67d774fd8e847861210bdf1a
hasta <mehrdad.shakibafar@gmail.com>
2020-11-11 13:01:36 +0330

    change api namespace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,7 +40,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace)
+                ->namespace($this->namespace.'API')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
