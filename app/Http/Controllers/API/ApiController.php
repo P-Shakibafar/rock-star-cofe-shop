315d1b8270174e06c96275e2fd9ad22f14ddf894
hasta <mehrdad.shakibafar@gmail.com>
2020-11-11 13:00:13 +0330

    create api controller for share same code

---
+++ app/Http/Controllers/API/ApiController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Traits\ApiResponser;
+use App\Http\Controllers\Controller;
+
+class ApiController extends controller {
+
+    use ApiResponser;
+}
